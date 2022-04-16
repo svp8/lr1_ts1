47758daec2ba3ed5fe7271319696b7e11f1d2c1e
TimurSadriev <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>
2022-04-17 00:55:25 +0500

    Наброски наброскию...

--- src/com/company/WorkFrame.java
+++ src/com/company/WorkFrame.java
@@ -4,7 +4,9 @@ import com.company.models.Client;
 import com.company.models.Request;
 
 import javax.swing.*;
-import javax.swing.table.TableModel;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -18,6 +20,8 @@ public class WorkFrame {
     MyImage myImage;
     JFrame frame;
     JPanel panel;
+    JLabel infoLabel;
+    String infoColor="<html><font color=\"red\" size=\"6\">";
     WorkFrame(int width,int height){
         this.width=width;
         this.height=height;
@@ -28,7 +32,7 @@ public class WorkFrame {
         requestArray.add(new Request(0,"04.04.2022","Сломалось колесо",1000,"10.04.2022",0));
         requestArray.add(new Request(1,"04.04.2021","Сломалось окно",11000,"10.04.2021",0));
         myImage=new MyImage(width,height,"src/com/company/Images/back.jpg");
-        dataClients = new String[100][4];
+        infoLabel=new JLabel();
     }
     public void start(){
 
@@ -36,31 +40,87 @@ public class WorkFrame {
         frame.setBounds(0,0,width,height);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setResizable(false);
+        frame.setLayout(null);
 
         panel=new JPanel();
         panel.setLayout(null);
+        panel.setBounds(0,0,width,height);
+
+        JPanel tablePanel=new JPanel();
+        tablePanel.setBounds(width*1/2,height/10,width*7/16,height*9/10);
+
+        frame.add(tablePanel);
         frame.add(panel);
 
-        updateClientTable();
+        infoLabel.setBounds(40,40,400,400);
+        panel.add(infoLabel);
+
+        final DefaultTableModel tableModel = new DefaultTableModel();
+        tableModel.addColumn("ID");
+        tableModel.addColumn("FIO");
+        tableModel.addColumn("Model");
+        tableModel.addColumn("Brand");
+        tableModel.addTableModelListener(e -> {
+            int row=e.getFirstRow();
+            clientArray.get(row).setId(Integer.parseInt(tableModel.getValueAt(row,0).toString()));
+            clientArray.get(row).setFIO(tableModel.getValueAt(row,1).toString());
+            clientArray.get(row).setBrand(tableModel.getValueAt(row,2).toString());
+            clientArray.get(row).setModel(tableModel.getValueAt(row,3).toString());
+            infoUpdate(row);
+        });
+        for(int i=0;i<clientArray.size();i++){
+            tableModel.insertRow(tableModel.getRowCount(), new Object[] {
+                    clientArray.get(i).getId(),
+                    clientArray.get(i).getFIO(),
+                    clientArray.get(i).getModel(),
+                    clientArray.get(i).getBrand()
+            });
+        }
+        tableClients = new JTable(tableModel);
+        JScrollPane scrollPane=new JScrollPane(tableClients);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        tablePanel.add(scrollPane);
+
         JButton buttonAddClient=new JButton("Добавит чела");
-        buttonAddClient.setBounds(400,400,100,30);
+        buttonAddClient.setBounds(100,400,100,30);
         panel.add(buttonAddClient);
-        buttonAddClient.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                createClient();
-            }
+        buttonAddClient.addActionListener(e -> {
+            createClient();
+            tableModel.insertRow(tableModel.getRowCount(), new Object[] {
+                   clientArray.get(clientArray.size()-1).getId(),
+                   clientArray.get(clientArray.size()-1).getFIO(),
+                   clientArray.get(clientArray.size()-1).getModel(),
+                   clientArray.get(clientArray.size()-1).getBrand()
+            });
         });
         JButton buttonDeleteClient=new JButton("Удалить чела");
-        buttonDeleteClient.setBounds(700,400,100,30);
+        buttonDeleteClient.setBounds(100,500,100,30);
         panel.add(buttonDeleteClient);
         buttonDeleteClient.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 deleteClient();
+                tableModel.removeRow(tableModel.getRowCount()-1);
             }
         });
 
+       Thread infoUpdater=new Thread(()->{
+           int prev=0,next=0;
+           while(true){
+               try {
+                   next=tableClients.getSelectedRow();
+               if(next!=prev){
+                   infoUpdate(next);
+                   prev=next;
+                   try {
+                       java.lang.Thread.sleep(500);
+                   } catch (InterruptedException e) {
+                       e.printStackTrace();
+                   }
+               }
+               } catch (ArrayIndexOutOfBoundsException e) {}
+       }});
+       infoUpdater.start();
 
         myImage.setBounds(0,0,width,height);
         panel.add(myImage);
@@ -68,34 +128,18 @@ public class WorkFrame {
         frame.setVisible(true);
     }
     public void createClient(){
-        String temp;
         Client tempClient=new Client();
-        tempClient.setFIO("kekkekekuuuraaaaas");
         clientArray.add(tempClient);
-        updateClientTable();
     }
     public void deleteClient(){
         clientArray.remove(0);
-        updateClientTable();
     }
-    public void updateClientTable(){
-        for(int k=0;k<100;k++){
-            dataClients[k][0]=" ";
-            dataClients[k][1]=" ";
-            dataClients[k][2]=" ";
-            dataClients[k][3]=" ";
-        }
-        int i=0;
-        for(Client client:clientArray){
-            dataClients[i][0]=Integer.toString(client.getId());
-            dataClients[i][1]=client.getFIO();
-            dataClients[i][2]=client.getBrand();
-            dataClients[i][3]=client.getModel();
-            i++;
-        }
-        String[] columnNames = {"id", "Fio", "Brand", "Model"};
-        tableClients = new JTable(dataClients, columnNames);
-        panel.add(tableClients);
-        tableClients.setBounds(10,200,300,300);
+    public void infoUpdate(int n){
+        Client client=clientArray.get(n);
+        infoLabel.setText(infoColor+n+":"+client.getId());
+        infoLabel.setText(infoLabel.getText()+ client.getFIO());
+        infoLabel.setText(infoLabel.getText()+" "+ client.getBrand());
+        infoLabel.setText(infoLabel.getText()+" "+client.getModel());
     }
 }
+
